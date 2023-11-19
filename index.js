e12c942f1adf5fd73054a46cae7e1977e6e5bbed
cebanoleksandr <100087075+cebanoleksandr@users.noreply.github.com>
2023-11-19 18:15:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const socketIO = require('socket.io');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const server = http.createServer(app);
-const io = socketIO(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-  },
-});
+const io = socketIO(server);
 
 let users = [];
 
